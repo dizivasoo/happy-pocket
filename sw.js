bd0331151bf752fa7140cb43acec24054f5fba5f
Harsh deep <jobzeroo42@gmail.com>
2026-02-15 08:52:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-const CACHE_NAME = "happy-pocket-v1";
+const CACHE_NAME = "happy-pocket-v2"; // Incremented version
 
 const filesToCache = [
-  "index.html",
-  "hp.css",
-  "hp.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+  "./",
+  "./index.html",
+  "./hp.css",
+  "./hp.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install Event - Caching the files
 self.addEventListener("install", event => {
+  self.skipWaiting(); // Forces the waiting service worker to become the active one
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(filesToCache);
@@ -17,6 +20,23 @@ self.addEventListener("install", event => {
   );
 });
 
+// Activate Event - Cleaning up old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event - Offline support
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -24,5 +44,3 @@ self.addEventListener("fetch", event => {
     })
   );
 });
-
-
